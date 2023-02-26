7a1957fd48cd1406f8104da45ddf7405117fa9a4
Vital Hoza <43417456+Mechneiy@users.noreply.github.com>
2023-02-26 14:52:28 +0300

    feat: change file js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,18 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
 
     }
 
     guess() {
+        return Math.ceil((this.max + this.min) / 2);
 
     }
 
     lower() {
+        this.max = this.guess();
 
     }
 
     greater() {
+        this.min = this.guess();
 
     }
 }
